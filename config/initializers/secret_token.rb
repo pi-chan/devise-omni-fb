89b12f39fa5355712e15b42e3b64e590d678eb7a
ohno <ohno@cflat-inc.com>
2013-06-07 10:30:51 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviseOmniFb::Application.config.secret_key_base = 'e241f7094c5e9d584b0241e3417ea7759bb9e27e84773a2b9829a75a98cdd2434bc6143b93ba75b9711812f23c4e462ecdaa81fb2305e2f9de650f5da1230135'
